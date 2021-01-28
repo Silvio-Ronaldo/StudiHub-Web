45f7e477de9b7009be9adaa2d1bdeb258dccc85b
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2021-01-28 19:22:23 -0300

    Criando roteamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyle from './styles/global';
+import theme from './styles/theme';
+
+import Routes from './routes';
 
 const App: React.FC = () => {
-  return <h1>Hello World!</h1>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Routes />
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 };
 
 export default App;
