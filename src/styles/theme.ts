82dc7b7b8a2be1a25aabd2d04c4b95964689b97a
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2021-01-28 19:10:47 -0300

    Definindo estilos iniciais

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    background: '#657786',
+    text: '#f5f8fa',
+    primary: '#1da1f2',
+    secondary: '#14171a',
+    lightBackground: '#aab8c2',
+    alternative: '#e1e8ed',
+    error: '#cc2200',
+    success: '#11dd11',
+  },
+};
+
+export default theme;
